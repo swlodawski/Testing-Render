18ef33a27c7e073479827eef890a5e69dc3a6967
sammy wlodawski <samwlodawski@gmail.com>
2024-06-25 14:14:31 -0400

    Index path change.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
+const api = require('./routes/index');
 
 const PORT = process.env.PORT || 3001;
 
